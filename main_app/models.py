ca1094082416922aa809a97c36c25925cdb1e4cc
Beruto <beruto000@gmail.com>
2016-11-23 00:29:57 +0100

    Almost done

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Treasure(models.Model):
+    # OldFashioned way
+    # def __init__(self, name: "String", value: "Int", material: "String", location: "Int"):
+    #     self.name = name
+    #     self.value = value
+    #     self.material = material
+    #     self.location = location
+    name = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    material = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    img_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
